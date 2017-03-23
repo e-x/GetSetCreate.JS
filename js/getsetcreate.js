d8b5347da0e8501716d94e1c0d8c5c13ded6f42c
e-x <gtihub>
2017-03-23 19:29:14 +0300

    comments added

--- js/getsetcreate.js
+++ js/getsetcreate.js
@@ -1,63 +1,4 @@
 
-
-function Create(e){
-    
-    return document.createElement(e);
-    
-}
-
-function SetID(e,val){    
-
-    if (e) { e.setAttribute("id",val); }
-
-}
-
-function Set(e,atr,val){    
-    var t=document.getElementById(e);
-    if (t)  { t.setAttribute(atr,val);  } 
-}
-
-function Get(e){ 
-    
-    var a = document.getElementById(e)
-    if ( a  ) { 
-        return a; 
-    } else { 
-        return 0;
-    }
-}
-
-function StoreSet(a,b){ 
-    window.localStorage.setItem(a,b); 
-}
-
-function StoreGet(a) { 
-    var e=window.localStorage.getItem(a); 
-    if ( e ) {
-        return e; 
-    }else { 
-        return ""; 
-    }
-}
-
-function SessSet(a,b){
-    
-    window.sessionStorage.setItem(a,b);
-    
-}
-
-function SessGet(a){
-    
-    var e=window.sessionStorage.getItem(a); 
-    if ( e ) {
-        return e; 
-    }else { 
-        return ""; 
-    }
-    
-}
- 
- 
 // Create element with setted type,id and class
  
 function Create(e,id,cls){    
@@ -65,40 +6,39 @@ function Create(e,id,cls){
     var a = document.createElement(e);
     if (id !="" && id != " " && id != "undefined" && id != null ) {a.setAttribute("id",id);}
     if (cls !="" && cls != " " && cls != "undefined" && cls != null ) {a.setAttribute("class",cls);}    
-     return a;
+    return a;
     
 }
 
-// Set element attribute
+// Set element attribute by ID
 
 function Set(e,atr,val){    
-    var t=document.getElementById(e); 
-    if (t)  { t.setAttribute(atr,val);  } 
+    var t=Get(e); 
+    if (t) { t.setAttribute(atr,val);  } 
 }
 
-// Get element 
+// Get element by ID
 
 function Get(e){ 
     var a = document.getElementById(e);
-    if ( a  ) { return a; }
-    else { return 0;}
+    if ( a ) { return a; }
+    else { console.log("Cant Get element " + a);
+        return "";}
 }
 
-// Delete element 
+// Delete element by ID
 
 function Del(e){
-    var a = Get(e);
-    
-    var p = a.parentNode;
-    console.log("DEL child " + a.id + " fom : parent " + p.id );
-    
-    p.removeChild(a);
-    
+    var a = Get(e); 
+    if ( a ) {
+        var p = a.parentNode;
+        p.removeChild(a);
+    }  
 }
 
-// Set focus 
+// Set focus by ID
 
-function FocusSet(elm){ var t = Get(elm); t.focus(); }
+function FocusSet(elm){ var t = Get(elm); if ( t ) { t.focus(); } }
 
 // Local and Sessions Store
 
